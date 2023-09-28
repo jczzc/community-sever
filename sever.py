58461c1c2c4a542748e6766b4964c1a138066728
jczzc <112162736+jczzc@users.noreply.github.com>
2023-09-28 22:21:11 +0800

    Add files via upload

--- sever.py
+++ sever.py
@@ -1,18 +1,13 @@
 import socket
 import pickle
-import copy
 import os
-import re
 import time
 import struct
 import json
-import requests
 import _thread
 import threading
 import traceback
-import hashlib
 import rsa
-import dill
 
 
 def split(long_message,public_key):
@@ -44,6 +39,10 @@ def waiters_manager():
 def handler(c,addr):
   global names
   global passwords
+  global bbs_end_code
+  global account_creatable
+  global executives
+  global banned_accounts
   try:
     print(addr)
     print('accept')
@@ -59,10 +58,15 @@ def handler(c,addr):
         c.send('F'.encode())
         c.close()
     elif con=='sign_up':
+      if not account_creatable:
+        c.close()
+        return False
       c.send(encoded_public_key_len)
       c.send(encoded_public_key)
       name_len=struct.unpack('L',c.recv(4))[0]
       name=rsa.decrypt(c.recv(name_len),private_key).decode()
+      if name in names:
+        c.close()
       key_lenth=struct.unpack('L',c.recv(4))[0]
       key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
       c.close()
@@ -82,7 +86,7 @@ def handler(c,addr):
       name=rsa.decrypt(c.recv(name_len),private_key).decode()
       key_lenth=struct.unpack('L',c.recv(4))[0]
       key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
-      if name not in names:
+      if (name not in names) or (name in banned_accounts):
         c.send(b'F')
         c.close()
         return False
@@ -99,12 +103,12 @@ def handler(c,addr):
       name=rsa.decrypt(c.recv(name_len),private_key).decode()
       key_lenth=struct.unpack('L',c.recv(4))[0]
       key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
-      if name not in names:
+      if (name not in names) or (name in banned_accounts):
         c.send(b'F')
         c.close()
         return False
       if passwords[name]==key:
-        c.send(b'T'.encode())
+        c.send(b'T')
         file_name_lenth=struct.unpack('L',c.recv(4))[0]
         file_name=c.recv(file_name_lenth).decode()
         file_piece_num=struct.unpack('L',c.recv(4))[0]
@@ -112,6 +116,7 @@ def handler(c,addr):
           for i in range(file_piece_num+1):
             lenth=struct.unpack('L',c.recv(4))[0]
             print(addr,':',lenth)
+            c.send(b'_')
             piece=c.recv(lenth)
             f.write(piece)
             #f.flush()
@@ -127,7 +132,7 @@ def handler(c,addr):
       name=rsa.decrypt(c.recv(name_len),private_key).decode()
       key_lenth=struct.unpack('L',c.recv(4))[0]
       key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
-      if name not in names:
+      if (name not in names) or (name in banned_accounts):
         c.send(b'F')
         c.close()
         return False
@@ -137,7 +142,7 @@ def handler(c,addr):
         author_name=c.recv(author_name_lenth).decode()
         file_name_lenth=struct.unpack('L',c.recv(4))[0]
         file_name=c.recv(file_name_lenth).decode()
-        if os.path.exists('files\\'+author_name+'\\'+file_name):
+        if os.path.exists('files\\'+author_name+'\\'+file_name) or (author_name in banned_accounts):
           c.send('T'.encode())
           file_size=os.path.getsize('files\\'+author_name+'\\'+file_name)
           size=piece_size=1024*500
@@ -160,6 +165,7 @@ def handler(c,addr):
               lenth=struct.pack('q',len(piece))
               c.send(lenth)
               time.sleep(0.02)
+              c.recv(1)
               c.send(piece)
               c.recv(1)
               print(addr,struct.unpack('Q',lenth)[0],len(lenth))
@@ -187,7 +193,7 @@ def handler(c,addr):
       name=rsa.decrypt(c.recv(name_len),private_key).decode()
       key_lenth=struct.unpack('L',c.recv(4))[0]
       key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
-      if name not in names:
+      if (name not in names) or (name in banned_accounts):
         c.send(b'F')
         c.close()
         return False
@@ -212,7 +218,7 @@ def handler(c,addr):
       name=rsa.decrypt(c.recv(name_len),private_key).decode()
       key_lenth=struct.unpack('L',c.recv(4))[0]
       key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
-      if name not in names:
+      if (name not in names) or (name in banned_accounts):
         c.send(b'F')
         c.close()
         return False
@@ -247,7 +253,7 @@ def handler(c,addr):
       name=rsa.decrypt(c.recv(name_len),private_key).decode()
       key_lenth=struct.unpack('L',c.recv(4))[0]
       key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
-      if name not in names:
+      if (name not in names) or (name in banned_accounts):
         c.send(b'F')
         c.close()
         return False
@@ -266,7 +272,7 @@ def handler(c,addr):
       name=rsa.decrypt(c.recv(name_len),private_key).decode()
       key_lenth=struct.unpack('L',c.recv(4))[0]
       key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
-      if name not in names:
+      if (name not in names) or (name in banned_accounts):
         c.send(b'F')
         c.close()
         return False
@@ -280,6 +286,363 @@ def handler(c,addr):
         lock.release()
         with open('passwords.json','w') as f:
           json.dump(passwords,f)
+      else:
+        c.send('F'.encode())
+        c.close()
+    elif con=='add_post':
+      c.send(encoded_public_key_len)
+      c.send(encoded_public_key)
+      name_len=struct.unpack('L',c.recv(4))[0]
+      name=rsa.decrypt(c.recv(name_len),private_key).decode()
+      key_lenth=struct.unpack('L',c.recv(4))[0]
+      key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
+      if (name not in names) or (name in banned_accounts):
+        c.send(b'F')
+        c.close()
+        return False
+      if passwords[name]==key:
+        c.send('T'.encode())
+        pack_len=struct.unpack('L',c.recv(4))[0]
+        pack=pickle.loads(c.recv(pack_len))
+        c.close()
+        topic,intro,content=pack
+        #bbs.append((topic,name))
+        path='posts'+os.sep+'{}-{}'.format(time.localtime().tm_mon,time.localtime().tm_mday)
+        file_name='{}-{}-{}.post'.format(name,topic,time.time())
+        if not os.path.exists(path):
+          os.mkdir(path)
+        with open(path+os.sep+file_name,'bw')as f:
+          f.write(pickle.dumps([topic,name,intro,content]))
+          f.flush()
+        lock.acquire()
+        try:
+          bbs.append([bbs_end_code,topic,name,path+os.sep+file_name])
+          with open('community_bbs.json','w') as f:
+            json.dump(bbs,f)
+          bbs_end_code+=1
+          with open('bbs_len.json','w') as f:
+            json.dump(bbs_end_code,f)
+            f.flush()
+          lock.release()
+        except Exception as e:
+          traceback.print_exc()
+          lock.release()
+        print(bbs)
+      else:
+        c.send('F'.encode())
+        c.close()
+    elif con=='ask_post':
+      c.send(encoded_public_key_len)
+      c.send(encoded_public_key)
+      name_len=struct.unpack('L',c.recv(4))[0]
+      name=rsa.decrypt(c.recv(name_len),private_key).decode()
+      key_lenth=struct.unpack('L',c.recv(4))[0]
+      key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
+      if (name not in names) or (name in banned_accounts):
+        c.send(b'F')
+        c.close()
+        return False
+      if passwords[name]==key:
+        c.send(b'T')
+        client_public_key_len=struct.unpack('L',c.recv(4))[0]
+        client_public_key=pickle.loads(c.recv(client_public_key_len))
+        #bbs
+        #bbs_end_code
+        client_pointer=struct.unpack('Q',c.recv(8))[0]
+        if client_pointer-10>=0:
+          bbs_list=bbs[client_pointer:client_pointer-10:-1]
+        elif client_pointer==0:
+          bbs_list=[bbs[0],]
+        else:
+          bbs_list=bbs[client_pointer:0:-1]
+        print(bbs_list)
+        encoded_bbs_list=pickle.dumps(bbs_list)
+        encrypted_bbs_list_len,encrypted_bbs_list=split(encoded_bbs_list,client_public_key)
+        c.send(encrypted_bbs_list_len)
+        c.send(encrypted_bbs_list)
+        c.close()
+      else:
+        c.send('F'.encode())
+        c.close()
+    elif con=='get_post':
+      c.send(encoded_public_key_len)
+      c.send(encoded_public_key)
+      name_len=struct.unpack('L',c.recv(4))[0]
+      name=rsa.decrypt(c.recv(name_len),private_key).decode()
+      key_lenth=struct.unpack('L',c.recv(4))[0]
+      key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
+      if (name not in names) or (name in banned_accounts):
+        c.send(b'F')
+        c.close()
+        return False
+      if passwords[name]==key:
+        c.send(b'T')
+        client_public_key_len=struct.unpack('L',c.recv(4))[0]
+        client_public_key=pickle.loads(c.recv(client_public_key_len))
+        post_code=struct.unpack('Q',c.recv(8))[0]
+        post_information=bbs[post_code]
+        path=post_information[3]
+        with open(path,'br') as f:
+          post=pickle.loads(f.read())
+        encoded_post=pickle.dumps(post)
+        encrypted_post_len,encrypted_post=split(encoded_post,client_public_key)
+        c.send(encrypted_post_len)
+        c.send(encrypted_post)
+        c.close()
+      else:
+        c.send('F'.encode())
+        c.close()
+    elif con=='get_bbs_end':
+      c.send(encoded_public_key_len)
+      c.send(encoded_public_key)
+      name_len=struct.unpack('L',c.recv(4))[0]
+      name=rsa.decrypt(c.recv(name_len),private_key).decode()
+      key_lenth=struct.unpack('L',c.recv(4))[0]
+      key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
+      if (name not in names) or (name in banned_accounts):
+        c.send(b'F')
+        c.close()
+        return False
+      if passwords[name]==key:
+        c.send(b'T')
+        c.send(struct.pack('Q',bbs_end_code-1))
+        c.close()
+      else:
+        c.send('F'.encode())
+        c.close()
+    elif con=='change_acc_creatable':
+      c.send(encoded_public_key_len)
+      c.send(encoded_public_key)
+      name_len=struct.unpack('L',c.recv(4))[0]
+      name=rsa.decrypt(c.recv(name_len),private_key).decode()
+      key_lenth=struct.unpack('L',c.recv(4))[0]
+      key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
+      if ((name not in names) or (name in banned_accounts)) or (True and (name not in admin)):
+        c.send(b'F')
+        c.close()
+        return False
+      if passwords[name]==key:
+        c.send(b'T')
+        account_creatable= not account_creatable
+        if account_creatable:
+          c.send(b'True ')
+        else:
+          c.send(b'False')
+        c.close()
+      else:
+        c.send('F'.encode())
+        c.close()
+    elif con=='ban_post':
+      c.send(encoded_public_key_len)
+      c.send(encoded_public_key)
+      name_len=struct.unpack('L',c.recv(4))[0]
+      name=rsa.decrypt(c.recv(name_len),private_key).decode()
+      key_lenth=struct.unpack('L',c.recv(4))[0]
+      key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
+      if (name not in names) or ((name not in executives) and (name not in admin)):
+        c.send(b'F')
+        c.close()
+        return False
+      if passwords[name]==key:
+        c.send(b'T')
+        post_index=struct.unpack('Q',c.recv(8))[0]
+        print('lock_bbs')
+        lock.acquire()
+        try:
+          bbs[post_index]=[post_index,'banned_post',name,bbs[post_index][3]]
+          with open(bbs[post_index][3],'wb') as f:
+            f.write(pickle.dumps([bbs[post_index][1],bbs[post_index][2],'This post is banned',b'This post is banned']))
+          with open('community_bbs.json','w') as f:
+            json.dump(bbs,f)
+          lock.release()
+        except:
+          traceback.print_exc()
+          lock.release()
+        c.close()
+      else:
+        c.send('F'.encode())
+        c.close()
+    elif con=='add_usr':
+      c.send(encoded_public_key_len)
+      c.send(encoded_public_key)
+      name_len=struct.unpack('L',c.recv(4))[0]
+      name=rsa.decrypt(c.recv(name_len),private_key).decode()
+      key_lenth=struct.unpack('L',c.recv(4))[0]
+      key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
+      if ((name not in names) or (name in banned_accounts)) or (name not in admin):
+        c.send(b'F')
+        c.close()
+        return False
+      if passwords[name]==key:
+        c.send(b'T')
+        new_usr_name_len=struct.unpack('L',c.recv(4))[0]
+        new_usr_name=rsa.decrypt(c.recv(new_usr_name_len),private_key).decode()
+        if new_usr_name in names:
+          c.send(b'F')
+          c.close()
+          return False
+        c.send(b'T')
+        new_usr_key_lenth=struct.unpack('L',c.recv(4))[0]
+        new_usr_key=rsa.decrypt(c.recv(new_usr_key_lenth),private_key).decode()
+        print('adim_adding_usr')
+        c.close()
+        lock.acquire()
+        try:
+          names.append(new_usr_name)
+          passwords[new_usr_name]=new_usr_key
+          with open('names.json','w') as f:
+            json.dump(names,f)
+          with open('passwords.json','w') as f:
+            json.dump(passwords,f)
+          os.mkdir('files\\'+name)
+          lock.release()
+        except:
+          traceback.print_exc()
+          lock.release()
+      else:
+        c.send('F'.encode())
+        c.close()
+    elif con=='add_executive':
+      c.send(encoded_public_key_len)
+      c.send(encoded_public_key)
+      name_len=struct.unpack('L',c.recv(4))[0]
+      name=rsa.decrypt(c.recv(name_len),private_key).decode()
+      key_lenth=struct.unpack('L',c.recv(4))[0]
+      key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
+      if ((name not in names) or (name in banned_accounts)) or (name not in admin):
+        c.send(b'F')
+        c.close()
+        return False
+      if passwords[name]==key:
+        c.send(b'T')
+        new_executive_name_len=struct.unpack('L',c.recv(4))[0]
+        new_executive_name=rsa.decrypt(c.recv(new_executive_name_len),private_key).decode()
+        if not new_executive_name in names:
+          c.send(b'F')
+          c.close()
+          return False
+        c.send(b'T')
+        c.close()
+        lock.acquire()
+        try:
+          cache=set(executives)
+          cache.add(new_executive_name)
+          executives=list(cache)
+          with open('executives.json','w') as f:
+            json.dump(executives,f)
+          print(executives)
+          lock.release()
+        except:
+          traceback.print_exc()
+          lock.release()
+      else:
+        c.send('F'.encode())
+        c.close()
+    elif con=='del_executive':
+      c.send(encoded_public_key_len)
+      c.send(encoded_public_key)
+      name_len=struct.unpack('L',c.recv(4))[0]
+      name=rsa.decrypt(c.recv(name_len),private_key).decode()
+      key_lenth=struct.unpack('L',c.recv(4))[0]
+      key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
+      if ((name not in names) or (name in banned_accounts)) or (name not in admin):
+        c.send(b'F')
+        c.close()
+        return False
+      if passwords[name]==key:
+        c.send(b'T')
+        new_executive_name_len=struct.unpack('L',c.recv(4))[0]
+        new_executive_name=rsa.decrypt(c.recv(new_executive_name_len),private_key).decode()
+        if not new_executive_name in executives:
+          c.send(b'F')
+          c.close()
+          return False
+        c.send(b'T')
+        c.close()
+        lock.acquire()
+        try:
+          cache=set(executives)
+          cache.remove(new_executive_name)
+          executives=list(cache)
+          with open('executives.json','w') as f:
+            json.dump(executives,f)
+          print(executives)
+          lock.release()
+        except:
+          traceback.print_exc()
+          lock.release()
+      else:
+        c.send('F'.encode())
+        c.close()
+    elif con=='ban_account':
+      c.send(encoded_public_key_len)
+      c.send(encoded_public_key)
+      name_len=struct.unpack('L',c.recv(4))[0]
+      name=rsa.decrypt(c.recv(name_len),private_key).decode()
+      key_lenth=struct.unpack('L',c.recv(4))[0]
+      key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
+      if ((name not in names) or (name in banned_accounts)) or (name not in admin):
+        c.send(b'F')
+        c.close()
+        return False
+      if passwords[name]==key:
+        c.send(b'T')
+        banned_name_len=struct.unpack('L',c.recv(4))[0]
+        banned_name=rsa.decrypt(c.recv(banned_name_len),private_key).decode()
+        if (not banned_name in names) or (banned_name in admin) or (banned_name in executives):
+          c.send(b'F')
+          c.close()
+          return False
+        c.send(b'T')
+        c.close()
+        lock.acquire()
+        try:
+          cache=set(banned_accounts)
+          cache.add(banned_name)
+          banned_accounts=list(cache)
+          with open('banned_accounts.json','w') as f:
+            json.dump(banned_accounts,f)
+          print(banned_accounts)
+          lock.release()
+        except:
+          traceback.print_exc()
+          lock.release()
+      else:
+        c.send('F'.encode())
+        c.close()
+    elif con=='unban_account':
+      c.send(encoded_public_key_len)
+      c.send(encoded_public_key)
+      name_len=struct.unpack('L',c.recv(4))[0]
+      name=rsa.decrypt(c.recv(name_len),private_key).decode()
+      key_lenth=struct.unpack('L',c.recv(4))[0]
+      key=rsa.decrypt(c.recv(key_lenth),private_key).decode()
+      if ((name not in names) or (name in banned_accounts)) or (name not in admin):
+        c.send(b'F')
+        c.close()
+        return False
+      if passwords[name]==key:
+        c.send(b'T')
+        unbanned_name_len=struct.unpack('L',c.recv(4))[0]
+        unbanned_name=rsa.decrypt(c.recv(unbanned_name_len),private_key).decode()
+        if (not unbanned_name in names) or (not unbanned_name in banned_accounts):
+          c.send(b'F')
+          c.close()
+          return False
+        c.send(b'T')
+        c.close()
+        lock.acquire()
+        try:
+          cache=set(banned_accounts)
+          cache.remove(unbanned_name)
+          banned_accounts=list(cache)
+          with open('banned_accounts.json','w') as f:
+            json.dump(banned_accounts,f)
+          print(banned_accounts)
+          lock.release()
+        except:
+          traceback.print_exc()
+          lock.release()
       else:
         c.send('F'.encode())
         c.close()
@@ -308,22 +671,29 @@ def sever(s):
       traceback.print_exc()
       print(e)
 
+admin=input('admin>>').split('|')
+
 lock=None
 lock = threading.Lock()
 waiters=[]
 public_key,private_key=rsa.newkeys(2048)
 encoded_public_key=pickle.dumps(public_key)
 encoded_public_key_len=struct.pack('L',len(encoded_public_key))
-public_key,private_key=rsa.newkeys(2048)
-encoded_public_key=pickle.dumps(public_key)
-encoded_public_key_len=struct.pack('L',len(encoded_public_key))
 chat_messages=[['system',"Hello_world!"],]
+account_creatable=False
 with open('names.json','r') as f:
   names=json.load(f)
 with open('passwords.json','r') as f:
   passwords=json.load(f)
+with open('community_bbs.json','r') as f:
+  bbs=json.load(f)
+with open('bbs_len.json','r') as f:
+  bbs_end_code=json.load(f)
+with open('executives.json','r') as f:
+  executives=json.load(f)
+with open('banned_accounts.json','r') as f:
+    banned_accounts=json.load(f)
 def main():
-
   #chat_messages=[['system',"Hello_world!"],]
   s=socket.socket(socket.AF_INET6,socket.SOCK_STREAM)
   s.bind((input('IPv6_address:'),1024))
